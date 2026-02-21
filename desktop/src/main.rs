f08dc1bb7d9e985cff31b852a47ead018bfa6497
omathot <omathot@proton.me>
2026-02-21 16:27:41 +0100

    added sound support for chip8 on desktop with sdl3 audio, needed to add a build.rs cause it stopped linking correctly for whatever reason

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,4 +1,5 @@
 use chip8_core::*;
+use sdl3::audio::{AudioCallback, AudioFormat, AudioSpec};
 use sdl3::event::Event;
 use sdl3::keyboard::Keycode;
 use sdl3::pixels::Color;
@@ -15,6 +16,67 @@ const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
 const TICK_PER_FRAME: usize = 5; // how many opcodes per frame to process
 
+struct SquareWave {
+	phase_inc: f32,
+	phase: f32,
+	volume: f32,
+}
+impl AudioCallback<f32> for SquareWave {
+	fn callback(&mut self, stream: &mut sdl3::audio::AudioStream, requested: i32) {
+		let mut out = Vec::<f32>::with_capacity(requested as usize);
+		for _ in 0..requested {
+			out.push(if self.phase <= 0.5 {
+				self.volume
+			} else {
+				-self.volume
+			});
+			self.phase = (self.phase + self.phase_inc) % 1.0;
+		}
+		let _ = stream.put_data_f32(&out);
+	}
+}
+
+pub struct AudioPlayer {
+	device: sdl3::audio::AudioStreamWithCallback<SquareWave>,
+	playing: bool,
+}
+
+impl AudioPlayer {
+	pub fn new(sdl_context: &sdl3::Sdl) -> Self {
+		let audio_subsystem = sdl_context.audio().unwrap();
+		let source_freq = 44100;
+		let spec = AudioSpec {
+			freq: Some(source_freq),
+			channels: Some(1),
+			format: Some(AudioFormat::f32_sys()),
+		};
+
+		let device = audio_subsystem
+			.open_playback_stream(
+				&spec,
+				SquareWave {
+					phase_inc: 440.0 / source_freq as f32,
+					phase: 0.0,
+					volume: 0.1,
+				},
+			)
+			.unwrap();
+		Self {
+			device,
+			playing: false,
+		}
+	}
+	pub fn set_beep(&mut self, should_beep: bool) {
+		if should_beep && !self.playing {
+			let _ = self.device.resume();
+			self.playing = true;
+		} else if !should_beep && self.playing {
+			let _ = self.device.pause();
+			self.playing = false;
+		}
+	}
+}
+
 fn main() {
 	let args: Vec<_> = env::args().collect();
 	if args.len() != 2 {
@@ -24,6 +86,7 @@ fn main() {
 
 	let sdl_context = sdl3::init().unwrap();
 	let video_subsystem = sdl_context.video().unwrap();
+	let mut audio = AudioPlayer::new(&sdl_context);
 	sdl3::hint::set(sdl3::hint::names::RENDER_VSYNC, "1");
 	let window = video_subsystem
 		.window("Chip-8 Emulator", WINDOW_WIDTH, WINDOW_HEIGHT)
@@ -72,6 +135,7 @@ fn main() {
 			chip8.tick();
 		}
 		chip8.tick_timers();
+		audio.set_beep(chip8.get_sound_timer() > 0);
 		draw_screen(&chip8, &mut canvas);
 	}
 }
